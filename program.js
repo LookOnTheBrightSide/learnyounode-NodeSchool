a86aee72acc5297d41e05e7d254b39c4900c186e
LookOnTheBrightSide <nkosindlovu87@gmail.com>
2015-02-27 15:43:57 +0200

    more tasks done

--- program.js
+++ program.js
@@ -1,12 +1,36 @@
-var fs = require('fs');
-fs.readFile(process.argv[2],'utf8', function(error, data){
-	if (error) {
-		return console.log('nada');
-	}else{
-		var contents = fs.readFileSync(process.argv[2]);
-		var splitToS = contents.toString();
-		var lines = splitToS.split('\n');
-			console.log(lines.length - 1);
-	}
-});
+// var fs = require('fs');
+// var path = require('path');
+// fs.fs.readdir(path, function(err,list){
+// 	if(err){
+// 		console.log('Something went wrong!');
+// 	}else{
+// 		console.log(list);
+// 	}
+// });
 
+var fs = require('fs');
+var path = require('path');
+
+var dir = process.argv[2];
+var ext = process.argv[3];
+
+function filter(file)
+{
+    if (path.extname(file) === ("." + ext)){
+            console.log(file);
+        }
+}
+fs.readdir(dir, function(err, list){
+    list.forEach(filter);
+ });
+
+
+
+// var fs = require('fs');
+// var path = require('path');
+// fs.readdir(process.argv[2], function (err, list) {
+//       list.forEach(function (file) {
+//         if (path.extname(file) === '.' + process.argv[3])
+//           console.log(file);
+//       });
+// });
